d3e7a0f4043be01abbee008f119cb325bb6d0378
Ubuntu <ubuntu@ip-172-31-10-52.ap-southeast-2.compute.internal>
2019-05-04 11:15:42 +0000

    rename folder to lower case letters

--- untitled/settings.py
+++ untitled/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 's7q3%yt&%a-)tesoay7h-99dy20n+8&=nzn)5oo1+kb0yw2=st'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

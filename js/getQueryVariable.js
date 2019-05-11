765b12089c38cfef3c0d401e1008c6654a5d0bb6
Yuanze Niu <yzniuGL@outlook.com>
2019-05-11 12:37:30 +1000

    header

--- js/getQueryVariable.js
+++ js/getQueryVariable.js
@@ -1,9 +1,18 @@
 
 
-function getQueryVariable(name) {
-    var result = window.location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
-    if (result == null || result.length < 1) {
-        return "";
-    }
-    return result[1];
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
+
+  
+//example:
+//http://www.runoob.com/index.php?id=1&image=awesome.jpg
+//getQueryVariable("id") returns 1
+// getQueryVariable("image") returns  "awesome.jpg"

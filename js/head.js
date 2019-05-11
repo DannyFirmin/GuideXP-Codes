5848e60dd5afed012ea42e329922236d35271fab
YuanzeNiu <yzniugl@outlook.com>
2019-05-12 00:28:00 +1000

    put function insert() in head.js

--- js/head.js
+++ js/head.js
@@ -1,3 +1,31 @@
+
+
+
+var exbi = new Array();
+exbi[0] = "Civic Art";
+exbi[1] = "Makers in Resdience"; 
+exbi[2] = "From the Hearts";
+exbi[3] = "Building Arts";
+
+
+function insert(){
+    var str = "";
+    var ul = document.getElementById("dropdowncontent");
+
+    for(var i = 0; i < exbi.length;i++){
+        var data=exbi[i];
+        var url = "exibition.html?exibition=" + data;
+        str= str + "<a href='"+ url + "'>"+data+"</a>";
+   
+        var li = document.createElement('li');
+        li.innerHTML = str;
+    }
+    ul.appendChild(li);
+ }
+                  
+
+
+
 document.writeln("<header class=\'site-navbar py-3 border-bottom\' role=\'banner\'>");
 document.writeln("");
 document.writeln("  <div class=\'container-fluid\'>");

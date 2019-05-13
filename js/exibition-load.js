1d4cf0e543b2aa954247b5d39549f85bcf9812aa
Danny Feng <u6611178@anu.edu.au>
2019-05-14 01:29:46 +1000

    Housing keeping

--- js/exibition-load.js
+++ js/exibition-load.js
@@ -12,12 +12,12 @@ if (url.indexOf("?") != -1) {
     }
 }
 
+// Create GUI from API, Synchronous method
 var request = new XMLHttpRequest();
 request.open('GET', 'http://admin.guidexp.me/api/exhibition/' + obj.exibition + '/', false); 
 request.send(null);
 if (request.status === 200) {
     var data = JSON.parse(request.responseText);
-
     for (i = 0; i < data.length; i++) {
             var lightGalleryDiv = document.getElementById('lightgallery');
             var imageCardDiv = document.createElement("div");
@@ -33,12 +33,11 @@ if (request.status === 200) {
             img.className = "img-fluid";
             a.appendChild(img);
             imageCardDiv.appendChild(a);
-            console.log("hi",lightGalleryDiv);
             lightGalleryDiv.appendChild(imageCardDiv);
         }
 }
+// Create resource from API, Asynchronous method
 const apiurl = 'http://admin.guidexp.me/api/exhibition/' + obj.exibition + '/'
-
 fetch(apiurl)
     .then(response => response.json())
     .then(data => {

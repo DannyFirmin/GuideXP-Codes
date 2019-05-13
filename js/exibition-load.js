14ff4cf1a38e4aecc71bed27f9a2965ad70d0d8f
Danny Feng <u6611178@anu.edu.au>
2019-05-14 00:42:36 +1000

    synchronous method is not working

--- js/exibition-load.js
+++ js/exibition-load.js
@@ -11,6 +11,57 @@ if (url.indexOf("?") != -1) {
         obj[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
     }
 }
+
+// var request = new XMLHttpRequest();
+// request.open('GET', 'http://admin.guidexp.me/api/exhibition/' + obj.exibition + '/', false); 
+// request.send(null);
+// if (request.status === 200) {
+//     var data = JSON.parse(request.responseText);
+
+//     for (i = 0; i < data.length; i++) {
+//         //     <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item" data-aos="fade" data-html="#audio0"
+//         //     data-sub-html="<h4>Fading Light</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor doloremque hic excepturi fugit, sunt impedit fuga tempora, ad amet aliquid?</p>">
+//         //     <a href="#"><img src="images/nature_small_1.jpg" alt="IMage" class="img-fluid"></a>
+//         //   </div>
+//             // load gui
+//             var lightGalleryDiv = document.getElementById('lightgallery');
+//             var imageCardDiv = document.createElement("div");
+//             imageCardDiv.className = "col-sm-6 col-md-4 col-lg-3 col-xl-2 item";
+//             imageCardDiv.setAttribute('data-aos', 'fade');
+//             imageCardDiv.setAttribute('data-html', '#audio' + i);
+//             imageCardDiv.setAttribute('data-sub-html', '<h4>' + data[i].art_name + '</h4><p>' + data[i].art_description + '</p>');
+//             a = document.createElement('a');
+//             a.setAttribute('href', '#');
+//             img = document.createElement('img');
+//             img.src = domain + "/" + data[i].art_img;
+//             img.setAttribute('alt', 'IMage');
+//             img.className = "img-fluid";
+//             a.appendChild(img);
+//             imageCardDiv.appendChild(a);
+//             console.log("hi",imageCardDiv);
+//             lightGalleryDiv.appendChild(imageCardDiv);
+
+//             // load audio resource
+//             var audioResDiv = document.createElement("div");
+
+//             audioResDiv.style = "display:none;";
+//             audioResDiv.id = 'audio' + i;
+
+//             audioTag = document.createElement('audio');
+//             audioTag.className = "lg-video-object lg-html5 video-js vjs-default-skin";
+//             audioTag.setAttribute('poster', domain + "/" + data[i].art_img);
+//             audioTag.setAttribute('preload', 'none');
+//             audioTag.setAttribute('controls', 'none');
+
+//             audioSource = document.createElement('source');
+//             audioSource.src = domain + "/" + data[i].art_audio;
+//             audioSource.setAttribute('type', 'audio/mpeg');
+//             audioTag.appendChild(audioSource);
+//             audioResDiv.appendChild(audioTag);
+//             var res = document.getElementById('res');
+//             res.appendChild(audioResDiv);
+//         }
+// }
 const apiurl = 'http://admin.guidexp.me/api/exhibition/' + obj.exibition + '/'
 
 fetch(apiurl)

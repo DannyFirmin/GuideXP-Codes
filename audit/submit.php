8a9c807574003c9efb357ff2579b46c66aab5ce0
Danny Feng <dannyf@diversityarrays.com>
2019-08-04 19:38:34 +1000

    Add php and html for request internal files access

---
+++ audit/submit.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+    Thanks, <?php echo $_POST["role"]; ?> <?php echo $_POST["name"]; ?><br>
+    Your submission has been recorded in our GuideXP Server. <br>
+    If you want to delete or edit it, please email to u6611178@anu.edu.au
+    <?php
+    $file_path = "submission.txt";
+    if (file_exists($file_path)) {
+        $fp = fopen($file_path, "w");
+        $str = $_POST["name"] . PHP_EOL . $_POST["email"] . PHP_EOL . $_POST["github"] . PHP_EOL . $_POST["role"];
+        fwrite($fp, $str);
+    }
+    fclose($fp);
+    ?>
+    <?php
+    $mydbhost = "localhost";
+    $mydbuser = "root";
+    $mydbpass = 'GuideXP123';
+    $conn = mysqli_connect($mydbhost, $mydbuser, $mydbpass);
+    if (!$conn) {
+        die("connect error: " . mysqli_error($conn));
+    }
+    mysqli_select_db($conn, 'audit_markers');
+    $sql = "INSERT INTO person (name, email, github_account, role)
+			VALUES
+			('$_POST[name]','$_POST[email]','$_POST[github]','$_POST[role]')";
+    $retval = mysqli_query($conn, $sql);
+    if (!$retval) {
+        die("create error" . mysqli_error($conn));
+    }
+    mysqli_close($conn);
+    ?>
+</body>
+
+</html>

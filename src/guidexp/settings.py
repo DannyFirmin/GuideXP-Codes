6d79780f24e23c10fcb8a7f05fd424217d800a93
leo <u6303287@anu.edu.au>
2019-09-14 15:11:36 +1000

    aws database configure

--- src/guidexp/settings.py
+++ src/guidexp/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'guidexp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guidexp',
+        'USER': 'admin',
+        'PASSWORD': 'GuideXP123',
+        'HOST': 'testdatabase.c91dnhczkfpo.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 

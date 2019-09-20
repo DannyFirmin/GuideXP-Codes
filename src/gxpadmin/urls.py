8dfed7f6601e6c4b398f6a9a672c5d606a566873
qiuyu21 <38485507+qiuyu21@users.noreply.github.com>
2019-09-20 14:56:39 +1000

    Update urls.py

--- src/gxpadmin/urls.py
+++ src/gxpadmin/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import home_view,create_view, language_list_view, exhibition_list_view, exhibit_list_view, get_exhibit_view,search_result
+from .views import get_all_language_view, get_all_gallery_view, get_all_exhibition_from_gallery_view, \
+                   get_all_exhibit_from_exhibition_gallery, get_exhibit_from_exhibition_gallery, \
+                   search_all, search_exhibition_from_gallery_view, search_exhibiti_from_exhibition_gallery_view
 
 urlpatterns = [
-    path('',home_view),
-    path('create/', create_view),
-    path('api/language_list', language_list_view),
-    path('api/<slug:lan>/exhibition_list', exhibition_list_view),
-    path('api/<slug:lan>/<slug:exhibition_id>/exhibit_list', exhibit_list_view),
-    path('api/<slug:lan>/exhibit/<slug:exhibit_id>', get_exhibit_view),
-    path('api/<slug:lan>/search', search_result),
-
+    path('api/get/all_language', get_all_language_view),
+    path('api/get/<slug:lang_code>/all_gallery', get_all_gallery_view),
+    path('api/get/<slug:lang_code>/<slug:gallery_id>/all_exhibition', get_all_exhibition_from_gallery_view),
+    path('api/get/<slug:lang_code>/<slug:gallery_id>/<slug:exhibition_id>/all_exhibit', get_all_exhibit_from_exhibition_gallery),
+    path('api/get/<slug:lang_code>/<slug:gallery_id>/<slug:exhibition_id>/<slug:exhibit_id>', get_exhibit_from_exhibition_gallery),
+    path('api/search/<slug:lang_code>/<slug:search_item>', search_all),
+    path('api/search_exhibition/<slug:lang_code>/<slug:gallery_id>/<slug:search_item>', search_exhibition_from_gallery_view),
+    path('api/search_exhibit/<slug:lang_code>/<slug:gallery_id>/<slug:exhibition_id>/<slug:search_item>', search_exhibiti_from_exhibition_gallery_view),
 ]
+

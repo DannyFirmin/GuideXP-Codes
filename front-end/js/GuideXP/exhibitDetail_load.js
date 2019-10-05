43d2be7e582b5e44b1e078cf4c479b062330d99e
YuanzeNiu <yzniugl@outlook.com>
2019-10-05 15:35:30 +1000

    detail page navbar fix

--- front-end/js/GuideXP/exhibitDetail_load.js
+++ front-end/js/GuideXP/exhibitDetail_load.js
@@ -1,45 +1,3 @@
-//Set cookie
-const available_language_list = ['en','zh','ja','ko','es','fr'];
-
-function setCookie(cname, cvalue, exdays, reload) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-
-    if (reload){
-        window.location.reload();
-    }
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie(){
-    let lang = getCookie("lang");
-    if (lang === ""){
-        let [lang, locale] = (((navigator.userLanguage || navigator.language).replace('-', '_')).toLowerCase()).split('_');
-        let i = available_language_list.indexOf(lang);
-        if (i !== -1){
-            setCookie("lang",lang,1, false);
-        }else{
-            setCookie("lang",'en',1, false);
-        }
-    }
-}
 checkCookie();
 
 //Get value from url
@@ -54,7 +12,7 @@ if (url.indexOf("?") != -1) {
     }
 }
 
-var api_url =  'http://13.239.36.190/api/get_exhibit/'+getCookie('lang')+obj.exhibit_id;
+//var api_url =  'http://13.239.36.190/api/get_exhibit/'+getCookie('lang')+obj.exhibit_id;
 
 
 var translation_menu;
